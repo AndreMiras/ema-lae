1aee1788385a0d497a22c510a216a983c1ea5493
Andre Miras <andre.miras@gmail.com>
2011-10-01 16:45:49 +0200

    User DAO implementation

--- EmaLaeDesktopApplication/src/dao/UserDao.java
+++ EmaLaeDesktopApplication/src/dao/UserDao.java
@@ -5,10 +5,32 @@
 
 package dao;
 
+import database.entity.User;
+
 /**
  *
  * @author andre
  */
-public class UserDao implements IDao<Object> {
+public class UserDao extends DaoHibernate<User> {
+
+    public User create(User obj)
+    {
+        return (User) getSession().save(obj);
+    }
+
+    public User read(long id)
+    {
+        return (User) getSession().get(User.class, id);
+    }
+
+    public void update(User obj)
+    {
+        getSession().update(obj);
+    }
+
+    public void delete(User obj)
+    {
+        getSession().delete(obj);
+    }
 
 }
