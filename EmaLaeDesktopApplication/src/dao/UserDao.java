5d36a20197b143a2c14f08bc13e9a5ccbe88d620
Andre Miras <andre.miras@gmail.com>
2011-10-01 15:12:14 +0200

    User DAO work in progress

---
+++ EmaLaeDesktopApplication/src/dao/UserDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+/**
+ *
+ * @author andre
+ */
+public class UserDao implements IDao<Object> {
+
+}
