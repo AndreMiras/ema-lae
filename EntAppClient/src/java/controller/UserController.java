4cd200f80b97f09d5c0a4bbba19fe3184984188f
Andre Miras <andre.miras@gmail.com>
2011-10-12 10:40:37 +0200

    client side Swing application user creation/retrieve

---
+++ EntAppClient/src/java/controller/UserController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import ejb.UserManagerSessionBeanRemote;
+import entity.UserLae;
+import java.util.Collection;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author amiras
+ */
+public class UserController
+{
+    @EJB
+    private static UserManagerSessionBeanRemote userManager;
+
+    public void createUser(String username, String password)
+    {
+        userManager.addUser(username, password);
+    }
+    
+    public Collection<UserLae> getAllUsers(String username, String password)
+    {
+        return userManager.getAllUsers();
+    }
+}
