937e9390d69b914b6c399897bcd789c9b646e392
Andre Miras <andre.miras@gmail.com>
2011-10-30 23:43:20 +0100

    started UserProfile Dao

---
+++ EmaLaeDesktopApplication/src/dao/UserProfileDao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ *
+ * @author andre
+ */
+public class UserProfileDao extends DaoHibernate<UserProfileDao> {
+
+    public Integer create(UserProfileDao obj)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public UserProfileDao read(Integer id)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void update(UserProfileDao obj)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void delete(UserProfileDao obj)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<UserProfileDao> find(Hashtable<String, String> querySet)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public UserProfileDao get(Hashtable<String, String> querySet)
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<UserProfileDao> all()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
