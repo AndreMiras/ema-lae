21171e8669fc4276d720cf4f7e774d9000463873
Andre Miras <andre.miras@gmail.com>
2011-11-12 18:41:06 +0100

    removed deprecated commented code

--- EmaLaeDesktopApplication/src/database/util/HibernateUtil.java
+++ EmaLaeDesktopApplication/src/database/util/HibernateUtil.java
@@ -32,19 +32,6 @@ public class HibernateUtil {
         }
     }
 
-    /*static {
-        try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            // sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }*/
-
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
