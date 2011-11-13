001d7945ee2ff60ce38ffbf75b76a81e2ef7e1dc
Andre Miras <andre.miras@gmail.com>
2011-11-13 20:07:22 +0100

    admin main, controller work

---
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/controller/AdminMainController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package emalaedesktopapplication.controller;
+
+import emalaedesktopapplication.EmaLaeDesktopView;
+import emalaedesktopapplication.forms.AdminMainPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author andre
+ */
+public class AdminMainController
+{
+    private EmaLaeDesktopView mainWindow;
+    private AdminMainPanel view;
+
+    public AdminMainController(EmaLaeDesktopView mainWindow, AdminMainPanel view)
+    {
+        this.mainWindow = mainWindow;
+        this.view = view;
+
+        // TODO: Add listeners to the view.
+        // view.addConnectionButtonListener(new TodoButtonListener());
+    }
+
+    /**
+     * Tries to log the user in using given credential
+     */
+    class TodoButtonListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("action performed");
+        }
+    }
+}
