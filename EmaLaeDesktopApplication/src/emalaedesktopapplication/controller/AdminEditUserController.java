5f777d1c7df2ac9b6e0f9ebebf88eda8536ac5f5
Andre Miras <andre.miras@gmail.com>
2011-11-23 22:06:56 +0100

    added missing admin edit user controller

---
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/controller/AdminEditUserController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package emalaedesktopapplication.controller;
+
+import client.RmiClient;
+import database.entity.User;
+import emalaedesktopapplication.EmaLaeDesktopView;
+import emalaedesktopapplication.forms.admin.AdminEditUserPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author andre
+ */
+public class AdminEditUserController {
+
+    private EmaLaeDesktopView mainWindow;
+    private AdminEditUserPanel view;
+
+    public AdminEditUserController(EmaLaeDesktopView mainWindow, AdminEditUserPanel view)
+    {
+        this.mainWindow = mainWindow;
+        this.view = view;
+
+        view.addSaveButtonListener(new SaveListener());
+    }
+
+    class SaveListener implements ActionListener
+    {
+
+        public void actionPerformed(ActionEvent e)
+        {
+            User editedUser = view.save();
+            try
+            {
+                // hit the database back with the edited user
+                RmiClient.getController().updateUser(editedUser);
+            } catch (RemoteException ex)
+            {
+                Logger.getLogger(AdminEditUserController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+}
