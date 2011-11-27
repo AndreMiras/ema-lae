e71ac00d48d4e4dc8ff656c087f0a95b394be258
Andre Miras <andre.miras@gmail.com>
2011-11-27 13:41:25 +0100

    explicitly deleting user from groups before deleting user

--- EmaLaeDesktopApplication/src/dao/UserDao.java
+++ EmaLaeDesktopApplication/src/dao/UserDao.java
@@ -1,6 +1,9 @@
 package dao;
 
+import database.entity.Group;
 import database.entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -16,5 +19,21 @@ public class UserDao extends DaoHibernate<User, Integer>
     public UserDao(Class<User> type)
     {
         super(type);
+    }
+
+    @Override
+    public void delete(User user)
+    {
+        Session session = sessionFactory.openSession(); // = getSession();
+        Transaction transaction = session.beginTransaction();
+        for (Group group : user.getGroups())
+        {
+            group.getUsersId().remove(user);
+            session.update(group);
+        }
+        // em.remove(user);
+        session.delete(user);
+        transaction.commit();
+        session.close();
     }
 }
