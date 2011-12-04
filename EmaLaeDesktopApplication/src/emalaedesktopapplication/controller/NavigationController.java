fcc7ab0e479853fdc6fb13cd317f56c25e5881b5
Andre Miras <andre.miras@gmail.com>
2011-12-04 04:49:52 +0100

    navigation controller

---
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/controller/NavigationController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emalaedesktopapplication.controller;
+
+import emalaedesktopapplication.EmaLaeDesktopView;
+import emalaedesktopapplication.forms.NavigationPanel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.tree.TreePath;
+
+/**
+ *
+ * @author andre
+ */
+public class NavigationController
+{
+
+    private EmaLaeDesktopView mainWindow;
+    private NavigationPanel navigationPanel;
+
+    public NavigationController(EmaLaeDesktopView mainWindow,
+            NavigationPanel navigationPanel)
+    {
+        this.mainWindow = mainWindow;
+        this.navigationPanel = navigationPanel;
+
+        // Add listeners to the view.
+        navigationPanel.addTreeMouseListener(new TreeMouseListener());
+    }
+
+    /**
+     * Tries to log the user in using given credential
+     */
+    class TreeMouseListener implements MouseListener
+    {
+
+        public void mouseClicked(MouseEvent me)
+        {
+            // TreePath tp = tree.getPathForLocation(me.getX(), me.getY());
+            TreePath tp = navigationPanel.getCurrentPath();
+            System.out.println("Mouse clicked:");
+            if (tp != null)
+            {
+                System.out.println(tp.toString());
+            }
+        }
+
+        public void mousePressed(MouseEvent me)
+        {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        public void mouseReleased(MouseEvent me)
+        {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        public void mouseEntered(MouseEvent me)
+        {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        public void mouseExited(MouseEvent me)
+        {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+}
