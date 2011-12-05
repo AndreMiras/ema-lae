a63be17c3d229a50c671548f5b9559a35682083e
Simon Boeuf <sboeuf@gmail.com>
2011-12-05 17:10:01 +0100

    small change

--- EmaLaeDesktopApplication/src/emalaedesktopapplication/utils/Utils.java
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/utils/Utils.java
@@ -18,7 +18,7 @@ public class Utils
     public static String getClassNameWithoutPackage(Class cls)
     {
         String clsname = cls.getName();
-        System.out.println("Full class name =" + clsname);
+        System.out.println("Full class name = " + clsname);
         int mid = clsname.lastIndexOf('.') + 1;
         String finalClsName = clsname.substring(mid);
         return finalClsName;
