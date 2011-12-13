ebecfea63e22846c97d0428e22666dcec3de55dd
Andre Miras <andre.miras@gmail.com>
2011-12-13 17:02:21 +0100

    admin many-to-many layout work

--- EmaLaeDesktopApplication/src/emalaedesktopapplication/forms/admin/ManyToManySelectorPanel.java
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/forms/admin/ManyToManySelectorPanel.java
@@ -143,7 +143,10 @@ public class ManyToManySelectorPanel<T extends WithPrimaryKey>
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
+        allObjectsListWidget.setMaximumSize(new java.awt.Dimension(45, 45));
+        allObjectsListWidget.setMinimumSize(new java.awt.Dimension(45, 45));
         allObjectsListWidget.setName("allObjectsListWidget"); // NOI18N
+        allObjectsListWidget.setPreferredSize(new java.awt.Dimension(45, 45));
         jScrollPane1.setViewportView(allObjectsListWidget);
 
         jScrollPane2.setName("jScrollPane2"); // NOI18N
@@ -153,7 +156,10 @@ public class ManyToManySelectorPanel<T extends WithPrimaryKey>
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
+        selectedObjectsListWidget.setMaximumSize(new java.awt.Dimension(45, 45));
+        selectedObjectsListWidget.setMinimumSize(new java.awt.Dimension(45, 45));
         selectedObjectsListWidget.setName("selectedObjectsListWidget"); // NOI18N
+        selectedObjectsListWidget.setPreferredSize(new java.awt.Dimension(45, 45));
         jScrollPane2.setViewportView(selectedObjectsListWidget);
 
         org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(emalaedesktopapplication.EmaLaeDesktopApplication.class).getContext().getResourceMap(ManyToManySelectorPanel.class);
@@ -179,27 +185,27 @@ public class ManyToManySelectorPanel<T extends WithPrimaryKey>
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2)
-                    .addComponent(jButton1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 95, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                        .addComponent(jButton2)))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
