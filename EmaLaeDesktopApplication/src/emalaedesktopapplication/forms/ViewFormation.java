9118812f6f06157e03f1ee948decc9052269752e
Andre Miras <andre.miras@gmail.com>
2011-12-20 02:58:53 +0100

    view formation work, layout and hide the subformation tab when not relevant

--- EmaLaeDesktopApplication/src/emalaedesktopapplication/forms/ViewFormation.java
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/forms/ViewFormation.java
@@ -46,18 +46,25 @@ public class ViewFormation extends javax.swing.JPanel {
             viewSession = new ViewSession();
             viewSession.setSession(session);
             formationDetailsTabbedPane.addTab(
-                    session.toString(), viewSession);
+                    session.getType().name(), viewSession);
 
         }
-        
-        /*
-        selectedFormationListModel = new DefaultListModel();
-        for (Formation childFormation : formation.getChildrenFormations())
+
+        // only show the Sub formation tab if relevant
+        if (!formation.getChildrenFormations().isEmpty())
         {
-            selectedFormationListModel.addElement(childFormation.getName());
+            selectedFormationListModel = new DefaultListModel();
+            for (Formation childFormation : formation.getChildrenFormations())
+            {
+                selectedFormationListModel.addElement(childFormation.getName());
+            }
+            childrenFormationsList.setModel(selectedFormationListModel);
         }
-        childrenFormationsList.setModel(selectedFormationListModel);
-         */
+        else // otherwise hide/remove it
+        {
+            formationDetailsTabbedPane.removeTabAt(0);
+            subFormationPanel.setVisible(false);
+        }
 
         /*
          * FIXME : Sessions need to be implemented in business class
@@ -89,7 +96,7 @@ public class ViewFormation extends javax.swing.JPanel {
         formationNameLabel = new javax.swing.JLabel();
         formationNameValue = new javax.swing.JLabel();
         formationDetailsTabbedPane = new javax.swing.JTabbedPane();
-        jPanel2 = new javax.swing.JPanel();
+        subFormationPanel = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         childrenFormationsList = new javax.swing.JList();
 
@@ -148,7 +155,7 @@ public class ViewFormation extends javax.swing.JPanel {
 
         formationDetailsTabbedPane.setName("formationDetailsTabbedPane"); // NOI18N
 
-        jPanel2.setName("jPanel2"); // NOI18N
+        subFormationPanel.setName("subFormationPanel"); // NOI18N
 
         jScrollPane1.setBorder(null);
         jScrollPane1.setName("jScrollPane1"); // NOI18N
@@ -162,24 +169,24 @@ public class ViewFormation extends javax.swing.JPanel {
         childrenFormationsList.setName("childrenFormationsList"); // NOI18N
         jScrollPane1.setViewportView(childrenFormationsList);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout subFormationPanelLayout = new javax.swing.GroupLayout(subFormationPanel);
+        subFormationPanel.setLayout(subFormationPanelLayout);
+        subFormationPanelLayout.setHorizontalGroup(
+            subFormationPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(subFormationPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 378, Short.MAX_VALUE)
                 .addContainerGap())
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        subFormationPanelLayout.setVerticalGroup(
+            subFormationPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(subFormationPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
-        formationDetailsTabbedPane.addTab(resourceMap.getString("jPanel2.TabConstraints.tabTitle"), jPanel2); // NOI18N
+        formationDetailsTabbedPane.addTab(resourceMap.getString("subFormationPanel.TabConstraints.tabTitle"), subFormationPanel); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -205,8 +212,8 @@ public class ViewFormation extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addComponent(parentFormationPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(formationDetailsTabbedPane, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(formationDetailsTabbedPane, javax.swing.GroupLayout.DEFAULT_SIZE, 157, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         parentFormationPanel.getAccessibleContext().setAccessibleName(resourceMap.getString("parentFormationPanel.AccessibleContext.accessibleName")); // NOI18N
@@ -221,12 +228,12 @@ public class ViewFormation extends javax.swing.JPanel {
     private javax.swing.JTabbedPane formationDetailsTabbedPane;
     private javax.swing.JLabel formationNameLabel;
     private javax.swing.JLabel formationNameValue;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel parentFormationNameLabel;
     private javax.swing.JLabel parentFormationNameValue;
     private javax.swing.JPanel parentFormationPanel;
     private javax.swing.JLabel parentFormationParentLabel;
     private javax.swing.JLabel parentFormationParentValue;
+    private javax.swing.JPanel subFormationPanel;
     // End of variables declaration//GEN-END:variables
 }
