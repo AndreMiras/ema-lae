9195f4df43569e94d6bfba5ea4b779bcc9265a2a
Andre Miras <andre.miras@gmail.com>
2011-12-20 08:58:49 +0100

    view session layout enhancements, plus removed formation from the view (redundant information)

--- EmaLaeDesktopApplication/src/emalaedesktopapplication/forms/ViewSession.java
+++ EmaLaeDesktopApplication/src/emalaedesktopapplication/forms/ViewSession.java
@@ -32,8 +32,10 @@ public class ViewSession extends javax.swing.JPanel {
 
         if(session.getType() != null)
             typeValue.setText(session.getType().toString());
+        /* // formation is redundant
         if(session.getFormation() != null)
             formationValue.setText(session.getFormation().getName());
+         */
         if(session.getStartDate() != null)
             startDateValue.setText(session.getStartDate().toString());
         if (session.getEndDate() != null)
@@ -56,52 +58,97 @@ public class ViewSession extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        typeLabel = new javax.swing.JLabel();
-        startDateLabel = new javax.swing.JLabel();
-        endDateLabel = new javax.swing.JLabel();
-        formationLabel = new javax.swing.JLabel();
-        assignmentsLabel = new javax.swing.JLabel();
-        typeValue = new javax.swing.JLabel();
-        endDateValue = new javax.swing.JLabel();
-        startDateValue = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         assignmentsValue = new javax.swing.JLabel();
-        formationValue = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        startDateLabel = new javax.swing.JLabel();
+        startDateValue = new javax.swing.JLabel();
+        endDateLabel = new javax.swing.JLabel();
+        endDateValue = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        typeLabel = new javax.swing.JLabel();
         teacherLabel = new javax.swing.JLabel();
         teacherValue = new javax.swing.JLabel();
+        typeValue = new javax.swing.JLabel();
 
         org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(emalaedesktopapplication.EmaLaeDesktopApplication.class).getContext().getResourceMap(ViewSession.class);
         setBorder(javax.swing.BorderFactory.createTitledBorder(resourceMap.getString("Form.border.title"))); // NOI18N
         setName("Form"); // NOI18N
 
-        typeLabel.setText(resourceMap.getString("typeLabel.text")); // NOI18N
-        typeLabel.setName("typeLabel"); // NOI18N
-
-        startDateLabel.setText(resourceMap.getString("startDateLabel.text")); // NOI18N
-        startDateLabel.setName("startDateLabel"); // NOI18N
-
-        endDateLabel.setText(resourceMap.getString("endDateLabel.text")); // NOI18N
-        endDateLabel.setName("endDateLabel"); // NOI18N
-
-        formationLabel.setText(resourceMap.getString("formationLabel.text")); // NOI18N
-        formationLabel.setName("formationLabel"); // NOI18N
-
-        assignmentsLabel.setText(resourceMap.getString("assignmentsLabel.text")); // NOI18N
-        assignmentsLabel.setName("assignmentsLabel"); // NOI18N
-
-        typeValue.setText(resourceMap.getString("typeValue.text")); // NOI18N
-        typeValue.setName("typeValue"); // NOI18N
-
-        endDateValue.setText(resourceMap.getString("endDateValue.text")); // NOI18N
-        endDateValue.setName("endDateValue"); // NOI18N
-
-        startDateValue.setText(resourceMap.getString("startDateValue.text")); // NOI18N
-        startDateValue.setName("startDateValue"); // NOI18N
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(resourceMap.getString("jPanel1.border.title"))); // NOI18N
+        jPanel1.setName("jPanel1"); // NOI18N
 
         assignmentsValue.setText(resourceMap.getString("assignmentsValue.text")); // NOI18N
         assignmentsValue.setName("assignmentsValue"); // NOI18N
 
-        formationValue.setText(resourceMap.getString("formationValue.text")); // NOI18N
-        formationValue.setName("formationValue"); // NOI18N
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(assignmentsValue)
+                .addContainerGap(302, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(assignmentsValue)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(resourceMap.getString("jPanel2.border.title"))); // NOI18N
+        jPanel2.setName("jPanel2"); // NOI18N
+
+        startDateLabel.setText(resourceMap.getString("startDateLabel.text")); // NOI18N
+        startDateLabel.setName("startDateLabel"); // NOI18N
+
+        startDateValue.setText(resourceMap.getString("startDateValue.text")); // NOI18N
+        startDateValue.setName("startDateValue"); // NOI18N
+
+        endDateLabel.setText(resourceMap.getString("endDateLabel.text")); // NOI18N
+        endDateLabel.setName("endDateLabel"); // NOI18N
+
+        endDateValue.setText(resourceMap.getString("endDateValue.text")); // NOI18N
+        endDateValue.setName("endDateValue"); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(endDateLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                        .addComponent(endDateValue))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(startDateLabel)
+                        .addGap(18, 18, 18)
+                        .addComponent(startDateValue)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(startDateLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(startDateValue))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(endDateLabel)
+                    .addComponent(endDateValue))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(resourceMap.getString("jPanel3.border.title"))); // NOI18N
+        jPanel3.setName("jPanel3"); // NOI18N
+
+        typeLabel.setText(resourceMap.getString("typeLabel.text")); // NOI18N
+        typeLabel.setName("typeLabel"); // NOI18N
 
         teacherLabel.setText(resourceMap.getString("teacherLabel.text")); // NOI18N
         teacherLabel.setName("teacherLabel"); // NOI18N
@@ -109,77 +156,72 @@ public class ViewSession extends javax.swing.JPanel {
         teacherValue.setText(resourceMap.getString("teacherValue.text")); // NOI18N
         teacherValue.setName("teacherValue"); // NOI18N
 
+        typeValue.setText(resourceMap.getString("typeValue.text")); // NOI18N
+        typeValue.setName("typeValue"); // NOI18N
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(teacherLabel)
+                    .addComponent(typeLabel))
+                .addGap(33, 33, 33)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(typeValue)
+                    .addComponent(teacherValue))
+                .addContainerGap(37, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(teacherLabel)
+                    .addComponent(teacherValue))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(typeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(typeValue))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(12, 12, 12)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(teacherLabel)
-                                .addGap(6, 6, 6)
-                                .addComponent(teacherValue)
-                                .addGap(18, 18, 18)
-                                .addComponent(typeLabel)
-                                .addGap(30, 30, 30)
-                                .addComponent(typeValue))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(startDateLabel)
-                                .addGap(20, 20, 20)
-                                .addComponent(startDateValue)
-                                .addGap(18, 18, 18)
-                                .addComponent(endDateLabel)
-                                .addGap(28, 28, 28)
-                                .addComponent(endDateValue))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(formationLabel)
-                        .addGap(37, 37, 37)
-                        .addComponent(formationValue)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(assignmentsLabel)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(18, 18, 18)
-                        .addComponent(assignmentsValue)))
-                .addGap(18, 18, 18))
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(teacherLabel)
-                    .addComponent(teacherValue)
-                    .addComponent(typeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(typeValue))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(formationLabel)
-                    .addComponent(formationValue)
-                    .addComponent(assignmentsLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(assignmentsValue))
-                .addGap(34, 34, 34)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(endDateLabel)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(1, 1, 1)
-                        .addComponent(endDateValue))
-                    .addComponent(startDateLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(startDateValue))
-                .addGap(32, 32, 32))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel assignmentsLabel;
     private javax.swing.JLabel assignmentsValue;
     private javax.swing.JLabel endDateLabel;
     private javax.swing.JLabel endDateValue;
-    private javax.swing.JLabel formationLabel;
-    private javax.swing.JLabel formationValue;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JLabel startDateLabel;
     private javax.swing.JLabel startDateValue;
     private javax.swing.JLabel teacherLabel;
