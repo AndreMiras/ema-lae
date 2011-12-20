2362732f189c097c974631c7a87bf65406917017
Andre Miras <andre.miras@gmail.com>
2011-12-21 00:24:31 +0100

    project metrics

---
+++ scripts/metrics.sh
@@ -0,0 +1,22 @@
+projects="EmaLaeCommon  EmaLaeDesktopApplication  EmaLaeServerApplication"
+
+
+echo -e "== Projects line count ==\n"
+for project in $projects
+do
+    echo "$project lines:"
+    total=`find "$project/" -name "*.java" -type f -exec cat {} \; | wc -l`
+    tests=`find "$project/test/" -name "*.java" -type f -exec cat {} \; | wc -l`
+    echo -e "total: $total (tests: $tests)\n"
+done
+
+echo -e "\n"
+
+echo -e "== Projects class count ==\n"
+for project in $projects
+do
+    echo "$project classes:"
+    total=`find "$project/" -name "*.java" -type f -exec ls {} \; | wc -l`
+    tests=`find "$project/test" -name "*.java" -type f -exec ls {} \; | wc -l`
+    echo -e "total: $total (tests: $tests)\n"
+done
