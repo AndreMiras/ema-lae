16a92011cff023ec9d0c2339ac35fbae1b55d4e9
Andre Miras <andre.miras@gmail.com>
2011-12-21 00:26:02 +0100

    project metrics

--- scripts/metrics.sh
+++ scripts/metrics.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+# Compute some project metrics:
+#   - class count
+#   - line count
+
 projects="EmaLaeCommon  EmaLaeDesktopApplication  EmaLaeServerApplication"
 
+echo -e "== Projects class count ==\n"
+for project in $projects
+do
+    echo "$project classes:"
+    total=`find "$project/" -name "*.java" -type f -exec ls {} \; | wc -l`
+    tests=`find "$project/test" -name "*.java" -type f -exec ls {} \; | wc -l`
+    echo -e "total: $total (tests: $tests)\n"
+done
+
+echo -e "\n"
 
 echo -e "== Projects line count ==\n"
 for project in $projects
@@ -10,13 +25,4 @@ do
     echo -e "total: $total (tests: $tests)\n"
 done
 
-echo -e "\n"
 
-echo -e "== Projects class count ==\n"
-for project in $projects
-do
-    echo "$project classes:"
-    total=`find "$project/" -name "*.java" -type f -exec ls {} \; | wc -l`
-    tests=`find "$project/test" -name "*.java" -type f -exec ls {} \; | wc -l`
-    echo -e "total: $total (tests: $tests)\n"
-done
